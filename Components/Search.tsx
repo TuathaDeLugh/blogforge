2733934f75299b0bf20c0714f393080ed869f425
Umang Sailor <ursailor@gmail.com>
2024-03-04 23:00:00 +0530

    Style:searchbox design

--- Components/Search.tsx
+++ Components/Search.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
   return (
-    <div>Search</div>
+    <div className="m-5 flex  items-center justify-center gap-3">
+      <div className=" bg-gray-200 dark:bg-gray-700 p-4 rounded-lg w-full md:w-[30rem] ">
+        <form className="search-container inline-block relative w-4/5">
+      <input type="text" placeholder="Search Blog" name='search' className='bg-transparent w-full outline-none '/>
+      <button type="submit" className=' absolute right-5 top-0 bottom-0 opacity-50' ><FaSearch size={20} /></button>
+        </form>
+      <button className='border-l border-black dark:border-gray-500 w-1/5'>Filter</button>
+      </div>
+    </div>
   )
 }

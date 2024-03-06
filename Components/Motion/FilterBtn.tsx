b1e14f9b7dfc6db54bcf1ff5b06e16a73acadca2
Umang Sailor <ursailor@gmail.com>
2024-03-06 14:54:38 +0530

    style:  Update FilterDropDown component styles.

--- Components/Motion/FilterBtn.tsx
+++ Components/Motion/FilterBtn.tsx
@@ -40,7 +40,7 @@ const FilterDropDown = () => {
   ];
 
   return (
-    <div className="relative w-1/5 p-3 md:p-4 " ref={dropdownRef}>
+    <div className="relative w-1/5  py-3 md:py-4 " ref={dropdownRef}>
         <div
         className="border-l w-full border-black dark:border-gray-500 ">
       <motion.button className='w-full'
@@ -54,7 +54,7 @@ const FilterDropDown = () => {
       
       {open ? (
         <motion.ul
-        className=" bg-gray-300/70 dark:bg-slate-700/70 backdrop-blur mt-6 absolute right-0 list-none m-1 border overflow-y-auto max-h-60 md:max-h-96 dark:border-slate-700 w-max  rounded "
+        className=" bg-gray-300/70 dark:bg-slate-700/70 backdrop-blur-sm mt-6 absolute right-0 list-none m-1 border overflow-y-auto max-h-60 md:max-h-96 dark:border-slate-700 w-max  rounded "
         initial={{ opacity: 0, y: -20 , x:20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}

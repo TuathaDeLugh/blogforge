d9974366c0526eda611287ef91ab6e5fc1f9abd0
Umang Sailor <ursailor@gmail.com>
2025-03-09 11:47:04 +0530

    Revert "Merge pull request #5 from TuathaDeLugh/fix/error-page-rename"
    This reverts commit 3d60f1def5c1c5e6d4b617d5c25557ff6e812691, reversing
    changes made to 40cb3c7b031a4e507ac3bef9f6cf96208ee576be.

---
+++ app/error.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Div } from "@/Components/Motion/Motion";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+const ErrorComponent = () => {
+    const path = usePathname()
+    
+    return (<>
+         <section className="max-w-[1500px] mx-auto">
+                <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 min-h-[93vh] md:min-h-[91vh]  items-center flex justify-center flex-col lg:flex-row md:gap-28 gap-16">
+                    <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+                        <Div className="mx-auto"
+                            initial={{ opacity: 0, x: -40 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5, delay: 0.2 }}>
+
+                            <div>
+                                <div 
+                                className=" lg:absolute left-0 md:-top-16 lg:-top-20 text-[100px] lg:text-[200px] font-black text-slate-300 lg:opacity-30 lg:dark:opacity-10 -z-10 "
+                                >500</div>
+                                <div className="">
+                                    <h1 className="my-2 text-orange-400  font-bold text-2xl">
+                                        Looks like you&apos;ve lost the doorway
+                                    </h1>
+                                    <p className="my-2 text-gray-800 dark:text-gray-400">Sorry about that! Please visit our hompage or refresh.</p>
+                                    <div className="grid grid-flow-col grid-cols-2 w-[80%] gap-5">
+                                    <a href={path} className=" block my-2 border rounded md py-4 text-center bg-orange-400 text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-opacity-50">Refresh Page</a>
+                                    <a href="/" className=" block my-2 border rounded md py-4 text-center bg-orange-400 text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-700 focus:ring-opacity-50">GO Home!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Div>
+                    </div>
+                    <Div
+                            initial={{ opacity: 0, x: 40 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.5, delay: 0.2 }} className=" w-[50%] hidden md:flex md:justify-end  ">
+                        <Image width={400} height={400} alt="disconnected" src="https://firebasestorage.googleapis.com/v0/b/blog-forge-sailor.appspot.com/o/202012_07.jpg?alt=media&token=82f0ce91-8dd4-4d73-b126-e64351ef4877" className="rounded-full" />
+                    </Div>
+                </div>
+            </section>
+    </>);
+}
+
+export default ErrorComponent;
